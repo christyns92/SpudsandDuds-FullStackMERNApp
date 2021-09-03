529f99f4db67489b5eff877a88424202efc9a8e8
hkidd <harrisonakidd@gmail.com>
2021-09-03 13:42:43 -0500

    Delete button somewhat works, but crashes the page along with review deletion

--- client/src/components/UserReviewList/index.js
+++ client/src/components/UserReviewList/index.js
@@ -1,19 +1,64 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+import { useMutation } from "@apollo/client";
+import { REMOVE_REVIEW } from "../../utils/mutations";
+import { QUERY_REVIEWS } from "../../utils/queries";
+
+import Auth from "../../utils/auth";
 
 const UserReviewList = ({
   reviews,
   title,
   showTitle = true,
   showUsername = true,
-}) => {
+  }) => {
+
+  const [removeReview, { error }] = useMutation(REMOVE_REVIEW, {
+    update(cache, { data: { review } }) {
+      try {
+        const { reviews } = cache.readQuery({ query: QUERY_REVIEWS });
+
+        const newReviews = reviews.filter((r) => (r._id !== review._id));
+
+        cache.writeQuery({
+          query: QUERY_REVIEWS,
+          data: { reviews: [...newReviews] },
+        });
+
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  })
+
+  const removeCommentHandler = async (event, _id) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await removeReview({
+        variables: {
+          reviewId: _id
+        },
+      });
+
+      removeReview();
+
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   if (!reviews.length) {
     return <h3>No Reviews Yet</h3>;
   }
 
+
   return (
     <div>
       {showTitle && <h3>{title}</h3>}
+      {Auth.loggedIn() ? (
+        <>
       {reviews &&
         reviews.map((review) => (
           <div key={review._id} className="card mb-3">
@@ -39,8 +84,8 @@ const UserReviewList = ({
             <div className="card-body bg-light p-2">
               <p>{review.reviewText}</p>
             </div>
-            <button className="btn btn-default edit-review">Edit</button>
-            <button className="btn btn-default delete-review">Delete</button>
+            <button type="button" className="btn btn-default edit-review">Edit</button>
+            <button onClick={(event) => {removeCommentHandler(event, review._id)}} className="btn btn-default delete-review">Delete</button>
             <Link
               className="btn btn-primary btn-block btn-squared"
               to={`/reviews/${review._id}`}
@@ -49,6 +94,13 @@ const UserReviewList = ({
             </Link>
           </div>
         ))}
+        </>
+        ) : (
+          <p>
+            You need to be logged in to share your reviews. Please{' '}
+            <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+          </p>
+          )}
     </div>
   );
 };
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -11,7 +11,6 @@ import Auth from '../utils/auth';
 
 const Profile = () => {
   const { username: userParam } = useParams();
-  console.log({username: userParam});
 
   // If there is no `username` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
@@ -60,7 +59,7 @@ const Profile = () => {
             className="col-12 col-md-10 mb-3 p-3"
             style={{ border: '1px dotted #1a1a1a' }}
           >
-            <ReviewForm />
+            {/* <ReviewForm /> */}
           </div>
         )}
       </div>
