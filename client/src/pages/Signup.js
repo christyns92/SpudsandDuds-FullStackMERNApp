57d621c254b912c3bd9766c6283dcae326b7144e
Rico Perez <ricoperez1@gmail.com>
2021-09-06 11:23:45 -0500

    center login and signup

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -43,7 +43,7 @@ const Signup = () => {
   return (
     <main className="flex-row justify-center mb-4">
       <div className="col-12 col-lg-10">
-        <div className="card">
+        <div className="mx-auto card">
           <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
           <div className="card-body">
             {data ? (
