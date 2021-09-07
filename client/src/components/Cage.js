e9c3d1b5b5c9432e7d67df2374acc64a83237d4c
Christyn Garcia <christynsandusky@gmail.com>
2021-09-06 21:52:25 -0500

    incorporating Cage Potato

--- client/src/components/Cage.js
+++ client/src/components/Cage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import nick from '../images/cage.gif'
+import nicPotato from '../images/nicolascage.jpg'
 
 function Cage(){
     return(
-    <div id='cage-page'className="text-align-center">
+    <div id='cage-page'className="text-align-center justify-content-stretch">
         <h2>You've found the Cage Page</h2>
     <img className='cage-man' src={nick} alt='Nicholas Cage'/>
+    <img className='nicPotato' src={nicPotato} alt='NicPotato'/>
     </div>
     )
 }
