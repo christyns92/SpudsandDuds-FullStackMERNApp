4373669cd28b4903fe26d72a2e604a774937662f
Christyn Garcia <christynsandusky@gmail.com>
2021-09-06 21:23:35 -0500

    editing cage page styling

--- client/src/components/Cage.js
+++ client/src/components/Cage.js
@@ -3,7 +3,7 @@ import nick from '../images/cage.gif'
 
 function Cage(){
     return(
-    <div id='cage-page'>
+    <div id='cage-page'className="text-align-center">
         <h2>You've found the Cage Page</h2>
     <img className='cage-man' src={nick} alt='Nicholas Cage'/>
     </div>
