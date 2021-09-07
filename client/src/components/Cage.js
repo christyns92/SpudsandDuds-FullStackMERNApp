ec3783732cb2003ac26f85ea334d45c6c28d56a1
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2021-09-07 09:34:38 -0500

    clean up the cagetado

--- client/src/components/Cage.js
+++ client/src/components/Cage.js
@@ -4,10 +4,10 @@ import nicPotato from '../images/nicolascage.jpg'
 
 function Cage() {
     return (
-        <div className="text-align-center theMovieRundown">
+        <div className="text-align-center cageContainer theMovieRundown">
             <h2>You've found the Cage Page</h2>
 
-            <div className='text-align-center m-auto row'>
+            <div className='text-align-center cage m-auto row'>
                 <div id='cage-page' className='text-align-center col-12 pb-4'>
                     <img className='col-12' src={nick} alt='Nicholas Cage' />
 
