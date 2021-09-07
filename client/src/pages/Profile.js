2cc28b28f361eebc287d1cfb24f4b9ba0c10e490
hkidd <harrisonakidd@gmail.com>
2021-09-06 20:49:46 -0500

    removed console logs

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -17,9 +17,6 @@ const Profile = () => {
 
   const user = data?.me || data?.user || {};
 
-  console.log(user.username);
-  console.log(user.reviews);
-
   // redirect to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     return <Redirect to="/me" />;
